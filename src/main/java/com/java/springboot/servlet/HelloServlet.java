34503f02325e60ac9ab40011ec4c315e959ea37f
noobliuxg <517099914@qq.com>
2019-03-02 21:54:39 +0800

    spring-boot servlet

---
+++ src/main/java/com/java/springboot/servlet/HelloServlet.java
@@ -0,0 +1,32 @@
+package com.java.springboot.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "helloServlet",urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello Servlet Springboot");
+
+        PrintWriter writer = resp.getWriter();
+
+        writer.write("Hello Servlet Springboot");
+
+        writer.flush();
+
+        writer.close();
+    }
+}
